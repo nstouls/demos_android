c56bc032669e564dd165c09cf74b929663fc41f9
nstouls <nicolas.stouls@insa-lyon.fr>
2017-06-30 10:37:33 +0200

    Some internal files modified by Android Studio

--- Mywelcomemachine/app/src/main/java/com/welcoming_machines/mywelcomemachine/MainActivity.java
+++ Mywelcomemachine/app/src/main/java/com/welcoming_machines/mywelcomemachine/MainActivity.java
@@ -2,12 +2,19 @@ package com.welcoming_machines.mywelcomemachine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTexte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTexte = (TextView) findViewById(R.id.le_texte);
+        tvTexte.setText("Texte change-toi et danse avec la vie\n" +
+                "L'écho de ta voix est venu jusqu'à moi !");
     }
 }
